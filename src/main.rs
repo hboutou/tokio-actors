09441cd161f7f619b4005bcd0bcf88141b338ee6
hboutou <22304469+hboutou@users.noreply.github.com>
2023-07-23 03:14:23 -0400

    simple actor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::sync::{mpsc, oneshot};
+
+enum Message {
+    GetUniqueId { respond_to: oneshot::Sender<u32> },
+}
+
+struct Actor {
+    receiver: mpsc::Receiver<Message>,
+    next_id: u32,
+}
+
+impl Actor {
+    fn new(receiver: mpsc::Receiver<Message>) -> Self {
+        Actor { receiver, next_id: 0 }
+    }
+
+    fn handle_message(&mut self, msg: Message) {
+        match msg {
+            Message::GetUniqueId { respond_to } => {
+                self.next_id += 1;
+                let _ = respond_to.send(self.next_id);
+            }
+        }
+    }
+}
+
+async fn run_actor(mut actor: Actor) {
+    while let Some(msg) = actor.receiver.recv().await {
+        actor.handle_message(msg);
+    }
+}
+
+#[derive(Clone)]
+pub struct Handle {
+    sender: mpsc::Sender<Message>,
+}
+
+impl Handle {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel(8);
+        let actor = Actor::new(receiver);
+        tokio::spawn(run_actor(actor));
+
+        Self { sender }
+    }
+
+    pub async fn get_unique_id(&self) -> u32 {
+        let (send, recv) = oneshot::channel();
+        let msg = Message::GetUniqueId { respond_to: send };
+
+        let _ = self.sender.send(msg).await;
+        recv.await.unwrap()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let handle = Handle::new();
+
+    for _ in 0..10 {
+        let num = handle.get_unique_id().await;
+        println!("next value is {num}");
+    }
 }
